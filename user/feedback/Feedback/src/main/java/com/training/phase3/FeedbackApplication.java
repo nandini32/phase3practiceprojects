57309935975712525b8ef94b8d2bb73d5bd06dcc
nandini32 <58420860+nandini32@users.noreply.github.com>
2022-02-11 09:11:31 +0530

    Add files via upload

---
+++ user/feedback/Feedback/src/main/java/com/training/phase3/FeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.training.phase3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackApplication.class, args);
+	}
+
+}
